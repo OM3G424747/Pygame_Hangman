7ce47c56d1f91bd4e72638a44e00e363ed0f23c5
OM3G424747 <44314634+OM3G424747@users.noreply.github.com>
2021-03-03 19:41:24 +0200

    Update Hangman Game.py

---
+++ Hangman Game.py
@@ -0,0 +1,11 @@
+"""
+TO DO:
+
+Create a list with all letters from the alphabet 
+
+pop a letter from the list after it's guessed by a player or the CPU
+(create a main menu)set up the game for 2 player play or player VS CPU
+
+add a library of words for the game
+
+"""
