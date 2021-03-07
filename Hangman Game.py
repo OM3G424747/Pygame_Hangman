09ae5e7b44b82db44746f8c06f1c393dbb2cf0cc
OM3G424747 <44314634+OM3G424747@users.noreply.github.com>
2021-03-07 17:42:47 +0200

    Update Hangman Game.py
    Created outline for Game
    
    Created alphabet for player and AI input
    
    Created gamewindow size
    
    Created a Word list with "Test" words

--- Hangman Game.py
+++ Hangman Game.py
@@ -9,3 +9,72 @@ pop a letter from the list after it's guessed by a player or the CPU
 add a library of words for the game
 
 """
+
+import pygame
+import random
+
+Screen_Width = 1600
+Screen_Height = 800
+Screen_Title = "Hangman Game" #Sets name on gamewindow 
+White_Colour = (255,255,255) #colours set according to RGB - R 255 G 255  B 255
+Black_Colour = (0,0,0)
+Red_Colour = (255,0,0)
+Green_Colour = (0,255,0)
+Blue_Colour = (0,0,255)
+Grey_Colour = (100,100,100)
+LightGrey_Colour = (200,200,200)
+Clock = pygame.time.Clock()
+Alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+Word_List = ["Apple", "Ball"]
+
+
+
+class GameObject: #class for defining game objects that will be drawn onto the game screen and moved arround
+    def __init__(self, image_path, X_pos, Y_pos, Width, Height):
+        Object_Image = pygame.image.load(image_path) #Loads image to be set
+        self.Image = pygame.transform.scale(Object_Image, (Width, Height)) #Scales the image that's been loaded in 
+        self.X_pos = X_pos
+        self.Y_pos = Y_pos
+        self.Width = Width
+        self.Height = Height
+    
+    def Draw (self,background): #This function is used to draw the object on the game screen
+        background.blit(self.Image,(self.X_pos,self.Y_pos)) #Blit funtion is used to draw imaghes to the game screen/ surface selected along with the X and Y Pos taken in the form of a tuple 
+
+
+class Game:
+    Tick_Rate = 60 #Change to set framerate
+    
+    
+    def __init__ (self, title, width, height):
+        self.title = title
+        self.width = width
+        self.height = height 
+        self.Game_Screen = pygame.display.set_mode((width, height)) #Creates the window being displayed 
+        self.Game_Screen.fill(LightGrey_Colour) #Sets the default colour of the displayed window 
+        pygame.display.set_caption(title)
+
+    def run_game_loop (self):
+        Game_Over = False
+
+        while Game_Over == False:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    Game_Over = True
+                keys = pygame.key.get_pressed()  
+                if keys[pygame.K_UP] == True:
+                    Game_Over = True  
+                
+                self.Game_Screen.fill(Grey_Colour)
+
+            pygame.display.update() #Updates the current frame after completing the loop
+            Clock.tick(self.Tick_Rate) #Sets the frame rate per second
+
+pygame.init()
+
+new_game = Game(Screen_Title,Screen_Width,Screen_Height) #creates a new game part of the "game class"
+
+new_game.run_game_loop() #Starts the game loop as defined in the class to continue looping until the game over state becomes true 
+
+pygame.quit()
+quit()
